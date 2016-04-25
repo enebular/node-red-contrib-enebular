0469093924198afc85c12a56fdef2d1d84498126
Shuhei Hiya <syuhei176@gmail.com>
2016-04-25 13:22:39 +0900

    Updated deploy interface

--- enebular/lambda-deploy.js
+++ enebular/lambda-deploy.js
@@ -1,8 +1,11 @@
 var htmlspecialchars = require('htmlspecialchars');
 
+var not_include_list = ['node-red', 'node-red-node-aws-lambda-io', 'node-red-contrib-enebular']
+
 module.exports = function(RED) {
   console.log('##### eneublar lambda deploy ####');
   var ENEBULAR_URL = process.env.ISSUER || "https://enebular.com";
+  var flowId = process.env.FLOW_ID;
 
 	RED.httpAdmin.post("/enebular/deploy", function (req, res) {
     var flows = JSON.parse(req.body.flows);
@@ -24,7 +27,7 @@ module.exports = function(RED) {
       var modVer = types[node.type];
       if (modVer) {
         var module = modVer[0], version = modVer[1];
-        if (module !== 'node-red' && module !== 'node-red-node-aws-lambda-io') {
+        if (not_include_list.indexOf(module) < 0) {
           packages[module] = version;
         }
       }
@@ -34,9 +37,7 @@ module.exports = function(RED) {
     var formPostHtml =
       '<html><body onload="document.form1.submit()">' +
       '<form name="form1" method="POST" action="' + deployPostUrl + '">' +
-      '<input type="hidden" name="flows" value="' + htmlspecialchars(JSON.stringify(flows))+ '"/>' +
-      '<input type="hidden" name="creds" value="' + htmlspecialchars(JSON.stringify(creds))+ '"/>' +
-      '<input type="hidden" name="packages" value="' + htmlspecialchars(JSON.stringify(packages))+ '"/>' +
+      '<input type="hidden" name="flowId" value="' + flowId + '"/>' +
       '</form>' +
       '</body></html>';
     res.set('Content-Type', 'text/html');
